6225a3602c74d135203e38e4839b55338d73edb6
Wei Zhuo <wei.zhuo@ihandysoft.com>
2017-04-18 10:50:09 +0800

    add setup docs

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+"""
+Reston
+========
+
+Reston is a reverse engine framework.
+
+reverse android package and binary files.
+---------------------------------------------
+
+* DEX, ODEX
+* APK
+* Android's binary xml
+* Android resources
+* Disassemble DEX/ODEX bytecodes
+* Decompiler for DEX/ODEX files
+
+"""
 import sys
 import os
 
@@ -6,9 +23,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='reston',
-    description='Reston is a full python tool to play with Android files.',
+    description='Reston is a reverse engine framwork, current can play with Android files.',
+    long_description=__doc__,
     version='0.3',
+    url='http://github.com/zeaphoo/reston/',
     packages=find_packages(),
+    license='Apache',
+    author='zeaphoo',
+    author_email='zeaphoo@gmail.com',
     install_requires=['pyasn1', 'cryptography>=1.0', 'future', 'pygments'],
     setup_requires=['setuptools'],
 
